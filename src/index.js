51c476348024cd01024bfd0bf9902b12cfcb7792
Philipp Kastl <picastle@posteo.de>
2022-01-27 22:38:39 +0100

    reconstruct router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
-  <BrowserRouter>
-
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
